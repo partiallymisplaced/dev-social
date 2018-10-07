a4006ce4a11ad6456e506f3b1f8dee752d4239fa
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-10-06 23:17:59 -0700

    Adds mongoDB connection (with mongoose)

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-// Our first route
+// DB config 
+const db = require('./env').mongoURI;
 
+// Connect to mongoDB (mLab)
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+
+// Our first route
 app.get('/', (req, res) => res.send('Hello World'));
 
 const port = 9000;
