a65c24278fa59bc7152cc920a068a16522e2d4d4
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-10-08 00:49:59 -0700

    Adds method to create new user if one doesn't already exist

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,7 +8,21 @@ const User = require('../../models/User');
 // @access  Public
 
 router.post('/register', (req, res) => {
-    
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if(user){
+                return res.status(400).json({
+                    email: "A user with this email already exists"
+                });
+            } else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar: avatar,
+                    password: req.body.password
+              });  
+            }
+        })
 })
 
 module.exports = router;
