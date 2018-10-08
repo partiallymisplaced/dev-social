8589876ca91582a8fb0130237d2947c5e5c62866
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-10-08 01:26:44 -0700

    Imports gravatar into users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const gravatar = require('gravatar');
+
 const User = require('../../models/User');
 
 // @route   POST api/users/register
